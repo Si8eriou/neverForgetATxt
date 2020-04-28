ce76f562efd1ebb6afea370e5ac52a6057b1cfd8
Jacoby Murray <jacoby@tireguru.net>
2020-04-28 09:01:57 -0600

    wip

--- laravel/app/Http/Controllers/Auth/AuthController.php
+++ laravel/app/Http/Controllers/Auth/AuthController.php
@@ -4,12 +4,24 @@
 namespace App\Http\Controllers\Auth;
 
 
-use http\Env\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function saveNewUser(Request $request) {
-        dd('god is good');
-    }
+    public function saveNewUser(Request $request, $userID=false) {
+        if($userID) {
+            $user = User::find($userID);
+        }
+        else {
+            $user = new User();
+        }
 
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->password = $request->get('password');
+
+        $user->save();
+    }
 }
