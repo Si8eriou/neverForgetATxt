9b774df4b943511689daac7bc5a44260aefb529b
Jacoby Murray <jacoby@tireguru.net>
2020-07-02 17:40:57 -0600

    lots of changes with UI. Added some background pics. Change the contact table page.

--- app/src/app/auth/auth.routing.ts
+++ app/src/app/auth/auth.routing.ts
@@ -1,6 +1,8 @@
 import {LoginComponent} from "./login/login.component";
 import {AuthComponent} from "./auth/auth.component";
 import {RegisterComponent} from "./register/register.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {SettingsComponent} from "./settings/settings.component";
 
 export const AuthRouting = [
   {
@@ -15,5 +17,12 @@ export const AuthRouting = [
     path:  'registerForm',
     component: RegisterComponent
   },
-
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'userSettings',
+    component: SettingsComponent
+  }
 ]
--- app/src/app/never-forget-atext/contacts/user-contacts/user-contacts.component.ts
+++ app/src/app/never-forget-atext/contacts/user-contacts/user-contacts.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import {ContactService} from "../../../utilities/services/neverForgetAText/contact.service";
 import {Router} from "@angular/router";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-user-contacts',
   templateUrl: './user-contacts.component.html',
-  styleUrls: ['./user-contacts.component.scss']
+  styleUrls: ['./user-contacts.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
+
+
 export class UserContactsComponent implements OnInit {
+  expandedContact: ContactElement | null;
+  columnsToDisplay = ['fname', 'email', 'cell'];
+  columnsToDisplayNames = {
+    'fname' : 'Name',
+    'email' : 'E-Mail',
+    'cell' : 'Cell',
+  }
+
   public userContacts: any;
 
   constructor(private contactService: ContactService, private router: Router) { }
@@ -24,4 +42,10 @@ export class UserContactsComponent implements OnInit {
     this.router.navigate(['editContact'], {queryParams: {'cid': cid}})
   }
 
+}
+
+export interface ContactElement {
+  name: string;
+  email: string;
+  number: number;
 }
